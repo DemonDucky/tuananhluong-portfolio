cbc7798cf3d090c1190e89c576b2f5cee72ef319
Tuan Anh Luong <cuti1mat@gmail.com>
2024-10-10 16:18:52 +0700

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start">
+        <h1 className="text-7xl">Tuan Anh Luong</h1>
+        <h3 className="text-3xl w-full text-center">Coming Soon...</h3>
+      </main>
+    </div>
+  );
+}
